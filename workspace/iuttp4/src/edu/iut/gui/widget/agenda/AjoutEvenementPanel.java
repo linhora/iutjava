ce98c2e88f81581cc4386d433e24c06c81a1fe35
linhora <paulcordier22@gmail.com>
2015-12-10 19:33:07 +0100

    ajout marche pas : j'arrive pas a faire attendre la réponse de la fenetre a la fonction

--- workspace/iuttp4/src/edu/iut/gui/widget/agenda/AjoutEvenementPanel.java
+++ workspace/iuttp4/src/edu/iut/gui/widget/agenda/AjoutEvenementPanel.java
@@ -1,14 +1,14 @@
 package edu.iut.gui.widget.agenda;
 
 import java.awt.BorderLayout;
-import java.awt.EventQueue;
 import java.awt.FlowLayout;
 import java.awt.Font;
-
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.concurrent.atomic.AtomicBoolean;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
@@ -20,8 +20,10 @@ import javax.swing.text.DefaultCaret;
 import edu.iut.app.ExamEvent;
 
 public class AjoutEvenementPanel {
+	static AtomicBoolean flag= new AtomicBoolean(false);
 	public static ExamEvent creerEvenement(){
-		boolean flag=false;	
+        ExamEvent result=null;
+        //init();
         JFrame frame = new JFrame("Test");
         frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         try 
@@ -44,6 +46,61 @@ public class AjoutEvenementPanel {
         inputpanel.setLayout(new FlowLayout());
         JTextField input = new JTextField(20);
         JButton button = new JButton("Enter");
+        button.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				flag.set(true);
+			}
+		});
+        DefaultCaret caret = (DefaultCaret) textArea.getCaret();
+        caret.setUpdatePolicy(DefaultCaret.ALWAYS_UPDATE);
+        panel.add(scroller);
+        inputpanel.add(input);
+        inputpanel.add(button);
+        panel.add(inputpanel);
+        frame.getContentPane().add(BorderLayout.CENTER, panel);
+        frame.pack();
+        frame.setLocationByPlatform(true);
+        frame.setResizable(false);
+        //input.requestFocus();
+        frame.setVisible(true);
+        while(result == null)
+        	try{
+        		Thread.sleep(10);
+        	}
+			catch(InterruptedException e){}
+        return result;
+        
+	}
+	static void init(){
+		JFrame frame = new JFrame("Test");
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        try 
+        {
+           UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception e) {
+           e.printStackTrace();
+        }
+        JPanel panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+        panel.setOpaque(true);
+        JTextArea textArea = new JTextArea(15, 50);
+        textArea.setWrapStyleWord(true);
+        textArea.setEditable(false);
+        textArea.setFont(Font.getFont(Font.SANS_SERIF));
+        JScrollPane scroller = new JScrollPane(textArea);
+        scroller.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+        scroller.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+        JPanel inputpanel = new JPanel();
+        inputpanel.setLayout(new FlowLayout());
+        JTextField input = new JTextField(20);
+        JButton button = new JButton("Enter");
+        button.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				flag.set(true);
+			}
+		});
         DefaultCaret caret = (DefaultCaret) textArea.getCaret();
         caret.setUpdatePolicy(DefaultCaret.ALWAYS_UPDATE);
         panel.add(scroller);
@@ -55,7 +112,9 @@ public class AjoutEvenementPanel {
         frame.setLocationByPlatform(true);
         frame.setVisible(true);
         frame.setResizable(false);
-        input.requestFocus();
-		return null;
-	}
+        //input.requestFocus();
+        
+    }
+
 }
+
